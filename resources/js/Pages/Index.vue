8cc092dc9fbb854c2c78c78ca7ec7f5a97a4ee2a
Keneil-Samms <keneil@innovate10x.com>
2026-01-29 21:49:23 -0500

    inital commit

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Button from '@/Components/ui/button/Button.vue';
+
+</script>
+
+<template>
+    <h1 class="text-blue-500">
+  # Using npm
+npm init @eslint/config
+
+# This will ask you questions and create the config file
+    </h1>
+    <Button>
+      
+    </Button>
+</template>
