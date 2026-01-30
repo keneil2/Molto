0cdc61d4978a31931a572815591b759c3b90b0d2
Keneil-Samms <keneil@innovate10x.com>
2026-01-30 06:43:08 -0500

    chore:basic setup to start authentication feature developement

---
+++ app/Http/Authentication/AuthenticationController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Authentication;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\AuthenticationRequest;
+
+class AuthenticationController extends Controller
+{
+    public function register(AuthenticationRequest $request)
+    {
+        //
+    }
+
+    public function login(AuthenticationRequest $request)
+    {
+        //
+    }
+}
