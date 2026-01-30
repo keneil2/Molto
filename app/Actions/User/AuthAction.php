9f2052179ea6a8a399212e3ba7015cbb4b416483
Keneil-Samms <keneil@innovate10x.com>
2026-01-30 06:41:21 -0500

    CHORE:setting up to user dto and action patterns

---
+++ app/Actions/User/AuthAction.php
@@ -0,0 +1,9 @@
+<?php 
+namespace App\Actions\User;
+class AuthAction
+{
+    public function execute()
+    {
+        
+    }
+}
---
+++ app/Actions/User/CreateUserAction.php
@@ -0,0 +1,9 @@
+<?php 
+namespace App\Actions\User;
+
+class CreateUserAction{
+    public function execute()
+    {
+        
+    }
+}
